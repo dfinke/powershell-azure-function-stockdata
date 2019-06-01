b1c07c6543da2eaccb1bee6609ef08739002858d
dfinke <finked@hotmail.com>
2019-06-01 11:20:50 -0400

    Handle single ticker

--- StockData/run.ps1
+++ StockData/run.ps1
@@ -5,13 +5,13 @@ if (!$symbols) { $symbols = "msft,ibm,aapl,xom,ge,nflx" }
 
 $data = Invoke-RestMethod "https://api.iextrading.com/1.0/stock/market/batch?symbols=$($symbols)&types=quote&last=1"
 
-$data = $(
-    foreach ($name in $data.psobject.Properties.name) {
-        $data.$name.quote
-    }
-) | Select-Object Symbol, CompanyName, Sector, Open, Close, High, Low, YTDChange | ConvertTo-Json
+$result = @()
+foreach ($name in $data.psobject.Properties.name) {
+    $result += $data.$name.quote |
+    Select-Object Symbol, CompanyName, Sector, Open, Close, High, Low, YTDChange
+}
 
 Push-OutputBinding -Name Response -Value @{
     StatusCode = "ok"
-    Body       = $data
+    Body       = $result
 }
